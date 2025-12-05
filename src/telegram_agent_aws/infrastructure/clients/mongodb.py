2a4df9d82fad31d476a59cc8e6ef3f6aabcdab82
Tanmay Sharma <tanmays040@gmail.com>
2025-12-05 12:00:00 +0530

    clients setup

---
+++ src/telegram_agent_aws/infrastructure/clients/mongodb.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+
+from pymongo import MongoClient
+from telegram_agent_aws.config import settings
+
+
+@lru_cache(maxsize=1)
+def get_mongodb_client() -> MongoClient:
+    return MongoClient(settings.MONGODB_CONNECTION_STRING)
